7242eb97fbcfdb0f7d92fd0182da67743ab6a912
Dilligasx <aa.zia26@gmail.com>
2022-05-02 16:14:04 -0500

    "import_removed"

--- src/main/java/util/BrowserFactory.java
+++ src/main/java/util/BrowserFactory.java
@@ -3,7 +3,6 @@ package util;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
